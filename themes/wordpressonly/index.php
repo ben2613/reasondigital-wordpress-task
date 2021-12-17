db85b6087c0b377db40f357e9f32a95b63d3457a
ben2613 <sosgyuki@gmail.com>
2021-12-17 00:31:27 +0000

    For debugging

--- themes/wordpressonly/index.php
+++ themes/wordpressonly/index.php
@@ -14,23 +14,23 @@
  * @since Twenty Twenty-One 1.0
  */
 
-get_header(); ?>
+get_header();?>
 
-<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
-        <header class="page-header alignwide">
-                <h1 class="page-title"><?php single_post_title(); ?></h1>
-        </header><!-- .page-header -->
-<?php endif; ?>
+<?php if (is_home() && !is_front_page() && !empty(single_post_title('', false))): ?>
+  <header class="page-header alignwide">
+  <h1 class="page-title"><?php single_post_title();?></h1>
+  </header><!-- .page-header -->
+<?php endif;?>
 
 <?php
-if ( have_posts() ) {
-  while ( have_posts() ) {
+if (have_posts()) {
+  while (have_posts()) {
+    the_post();
+?>
 
-      the_post(); ?>
+  <h2><?php the_title();?></h2>
 
-      <h2><?php the_title(); ?></h2>
-
-      <?php the_content(); ?>
+  <?php the_content();?>
 
   <?php }
 }
