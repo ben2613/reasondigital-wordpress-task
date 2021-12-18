5f413762413c660164b2e49bda79d8f914502d53
ben2613 <sosgyuki@gmail.com>
2021-12-18 14:38:08 +0000

    Make it look better first

---
+++ themes/wordpressonly/footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying the footer
+ * Referring Twenty_Twenty_One
+ */
+
+?>
+      </main><!-- #main -->
+    </div><!-- #primary -->
+  </div><!-- #content -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
