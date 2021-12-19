add02bd5f4c39656bd1df2940d269296f14b86dd
ben2613 <sosgyuki@gmail.com>
2021-12-19 00:29:03 +0000

    Move header and fix site url

--- themes/wordpressonly/header.php
+++ themes/wordpressonly/header.php
@@ -19,34 +19,35 @@
 <div id="page" class="site">
 
   <div id="content" class="site-content">
-      <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
-        <header class="page-header alignwide">
-          <div id="icon-n-menu">
-            <label for="menu-toggle" id="menu-icon">
-              <div></div><div></div><div></div>
-            </label>
-            <?php if (has_site_icon()): ?>
-            <a href="<?php site_url(); ?>"><img src="<?php echo get_site_icon_url(40) ?>" alt="Site Icon"></a>
-            <?php endif; // has_site_icon ?>
+    <div id="primary" class="content-area">
+      <header class="page-header alignwide">
+        <div id="icon-n-menu">
+          <label for="menu-toggle" id="menu-icon">
+            <div></div><div></div><div></div>
+          </label>
+          <?php if (has_site_icon()): ?>
+          <a href="<?php bloginfo('url'); ?>"><img src="<?php echo get_site_icon_url(40) ?>" alt="Site Icon"></a>
+          <?php endif; // has_site_icon ?>
+        </div>
+        <input type="checkbox" id="menu-toggle">
+        <div class="menu-expand">
+          <div class="site-info">
+            <h1 class="site-title"><?php bloginfo('name');?></h1>
+            <h2 class="site-tagline"><?php bloginfo('description'); ?></h2>
           </div>
-          <input type="checkbox" id="menu-toggle">
-          <div class="menu-expand">
-            <div class="site-info">
-              <h1 class="site-title"><?php bloginfo('name');?></h1>
-              <h2 class="site-tagline"><?php bloginfo('description'); ?></h2>
-            </div>
-            <div class="cat-menu">
-            <?php
-              $menu_attrs = [
-                'menu' => 'primary_menu',
-                'echo' => false,
-                'depth' => 0,
-                'container' => false,
-                'items_wrap' => '%3$s',
-              ];
-              echo strip_tags(wp_nav_menu($menu_attrs), '<a>');
-            ?>
-            </div>
+          <div class="cat-menu">
+          <?php
+            $menu_attrs = [
+              'menu' => 'primary_menu',
+              'echo' => false,
+              'depth' => 0,
+              'container' => false,
+              'items_wrap' => '%3$s',
+            ];
+            echo strip_tags(wp_nav_menu($menu_attrs), '<a>');
+          ?>
           </div>
-        </header>
+        </div>
+      </header>
+
+      <main id="main" class="site-main" role="main">
