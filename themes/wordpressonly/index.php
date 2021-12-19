092558c42ed7e956a24c3f8d94b7126fb17602a2
ben2613 <sosgyuki@gmail.com>
2021-12-19 00:13:52 +0000

    Finish first page, with adding SCSS

--- themes/wordpressonly/index.php
+++ themes/wordpressonly/index.php
@@ -8,32 +8,7 @@
 
 get_header();?>
 
-<?php if (is_home() && !is_front_page() && !empty(single_post_title('', false))): ?>
-  <header class="page-header alignwide">
-    <h1 class="page-title"><?php single_post_title();?></h1>
-  </header><!-- .page-header -->
-<?php elseif(is_home() || is_category()): ?>
-  <header class="page-header alignwide">
-    <div class="siteInfo">
-      <h1 class="site-title"><?php bloginfo('name');?></h1>
-      <h2 class="site-tagline"><?php bloginfo('description'); ?></h2>
-    </div>
-    <div class="cat_menu">
-    <?php
-      $menu_attrs = [
-        'menu' => 'primary_menu',
-        'echo' => false,
-        'depth' => 0,
-        'container' => false,
-        'items_wrap' => '%3$s',
-      ];
-      echo strip_tags(wp_nav_menu($menu_attrs), '<a>');
-    ?>
-    </div>
-  </header><!-- .page-header -->
-<?php endif;?>
-
-  <div class="article-container">
+<div class="article-container">
 <?php
 if (have_posts()) {
   while (have_posts()) {
@@ -42,14 +17,18 @@ if (have_posts()) {
 ?>
 
   <article>
-    <img class="thumbnail" src="<?php echo htmlentities($thumbnail ? $thumbnail[0] : ''); ?>" />
-    <h3><?php the_title(); ?></h3>
+    <a href="<?php the_permalink(); ?>">
+      <img class="thumbnail" src="<?php echo htmlentities($thumbnail ? $thumbnail[0] : ''); ?>" />
+    </a>
+    <a href="<?php the_permalink(); ?>">
+      <h3 class="article-title"><?php the_title(); ?></h3>
+    </a>
     <?php the_category(', '); ?>
   </article>
 
   <?php }
 }
 ?>
-  </div> <!--article-container-->
+</div> <!--article-container-->
 <?php
 get_footer();
